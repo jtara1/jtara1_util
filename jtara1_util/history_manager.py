41a26f9b8265bb000f36548b0c5d643c546aa207
James T <jtara@tuta.io>
2018-02-23 17:54:17 -0800

    add: GeneralSchema class for which I use to abstract metadata; [wip] add: history_manager.py for which I use to keep track of some history information

---
+++ jtara1_util/history_manager.py
@@ -0,0 +1,154 @@
+import os
+import logging
+import json
+
+
+class History:
+    log_file = '.history.json'
+
+    @staticmethod
+    def history_log(dir=os.getcwd(), log_file='log_file.txt',
+                    mode='read', write_data=None):
+        """This should generally be called after another program has finished
+        to record it's progress or history.
+        Read python dictionary from or write python dictionary to a file
+
+        :param dir: directory for text file to be saved to
+        :param log_file: name of text file (include .txt extension)
+        :param mode: 'read', 'write', or 'append' are valid
+        :param write_data: data that'll get written in the log_file
+        :type write_data: dictionary (or list or set)
+
+        :return: returns data read from or written to file (depending on mode)
+        :rtype: dictionary
+        """
+        mode_dict = {
+            'read': 'r',
+            'write': 'w',
+            'append': 'a',
+            'update': 'r+',
+        }
+        if mode in mode_dict:
+            log_file_path = os.path.join(dir, log_file)
+            History.create_directories_if_needed(log_file_path)
+            if mode == 'update':
+                History.create_file_if_not_created(log_file_path, {})
+
+            with open(log_file_path, mode_dict[mode]) as f:
+                # read from file as JSON and return JSON
+                if mode == 'read':
+                    return json.loads(f.read())
+                # read data then update data with new write_data then save JSON
+                elif mode == 'update':
+                    data = json.loads(f.read())
+                    # only supporting updating if both are dictionaries
+                    if isinstance(data, dict) and isinstance(write_data, dict):
+                        data.update(write_data)
+                        f.seek(0)  # seek to beginning to overwrite data
+                        f.write(json.dumps(data))
+                    else:
+                        logging.critical(
+                            "[ccmixter_song_downloader.history_manager."
+                            "History.history_log] "
+                            "Does not support updating non-dict JSON")
+                    return data
+                # write or append modes
+                else:
+                    f.write(json.dumps(write_data))
+                    return write_data
+        else:
+            logging.debug('history_log func: invalid mode (param #3)')
+            return {}
+
+    @staticmethod
+    def get_previous_download_amount(tags, sort, dir, verbose=False):
+        """Open & update log_file to get last_id of subreddit of sort_type
+        Creates log_file if no existing log file was found
+
+        :param tags: name of subreddit
+        :param sort: sort type of subreddit
+        :param dir: directory log_file will be saved to
+        :param log_file: name of log file
+        :param verbose: prints extra messages
+
+        :return: log_data (contains last ids), last_id
+        :rtype: tuple
+
+        Note: this has been modified to work with ccmixter_song_downloader
+        """
+        no_history = False
+        log_data = None
+        last_id = None
+        try:
+            # first: we try to open the log_file
+            log_data = History.history_log(dir, History.log_file, 'read')
+
+            # second: we check if the data loaded is a dictionary
+            if not isinstance(log_data, dict):
+                raise ValueError(
+                    log_data,
+                    'data from %s is not a dictionary, overwriting %s'
+                    % (History.log_file, History.log_file))
+
+            # third: try loading last id for subreddit & sort_type
+            if tags in log_data:
+                if sort in log_data[tags]:
+                    last_id = log_data[tags][sort]['downloads']
+                else:  # sort not in log_data but tags is
+                    no_history = True
+                    log_data[tags][sort] = {'downloads': 0}
+            else:  # tags not listed as key in log_data
+                no_history = True
+                log_data[tags] = {sort: {'downloads': 0}}
+
+        # py3 or py2 exception for dne file
+        except (FileNotFoundError, IOError, FileExistsError):
+            last_id = ''
+            log_data = {
+                tags: {
+                    sort: {
+                        'downloads': 0
+                    }
+                }
+            }
+            History.history_log(dir, History.log_file, 'write', log_data)
+            if verbose:
+                print('%s not found in %s, created new %s'
+                      % (History.log_file, dir, History.log_file))
+
+        except ValueError as e:
+            if verbose:
+                print('log_data:\n{}'.format(e.args))
+
+        except Exception as e:
+            print(e)
+
+        if no_history:
+            last_id = 0
+            log_data = History.history_log(dir, History.log_file, 'write',
+                                           log_data)
+
+        return log_data, last_id
+
+    @staticmethod
+    def create_directories_if_needed(path, is_file=True):
+        """Creates the directories leading to the path if they don't exist
+
+        :param path: file path of a folder or file
+        :param is_file: should be True if path points to file, False otherwise
+        :return:
+        """
+        path = os.path.abspath(path)
+        if is_file:
+            path = os.path.dirname(path)
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
+    @staticmethod
+    def create_file_if_not_created(file_path, write_data={}):
+        try:
+            with open(file_path, 'r'):
+                pass
+        except (FileNotFoundError, FileExistsError):
+            with open(file_path, 'w') as f:
+                f.write(json.dumps(write_data))
---
+++ jtara1_util/serialization.py
@@ -0,0 +1,104 @@
+import json
+import os
+import dill
+import collections
+from pprint import pformat
+
+
+class Serialization:
+    def __init__(self, data_file):
+        self.data_file = os.path.abspath(data_file)
+        self.data = {}
+
+    @staticmethod
+    def make_paths_for_file(file_path, is_file=True):
+        """Make folders if needed for data file"""
+        path = os.path.dirname(file_path) \
+            if is_file else os.path.abspath(file_path)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    def serialize_as_json(self):
+        Serialization.make_paths_for_file(self.data_file)
+        json.dump(obj=self.data, fp=open(self.data_file, 'w'))
+
+    def deserialize_from_json(self, default=None):
+        try:
+            self.data = json.load(fp=open(self.data_file, 'r'))
+            return self
+        except (FileExistsError, FileNotFoundError):
+            print('[Serialization] WARNING: {} not found,\n'
+                  '\treturned default value: {}'.format(self.data_file, default))
+            return default
+
+    def serialize_as_binary(self):
+        Serialization.make_paths_for_file(self.data_file)
+        dill.dump(obj=self.data, file=open(self.data_file, 'wb'))
+
+    def deserialize_from_binary(self, default=None):
+        try:
+            self.data = dill.load(file=open(self.data_file, 'rb'))
+            return self
+        except (FileExistsError, FileNotFoundError):
+            print('[Serialization] WARNING: {} not found,\n'
+                  '\treturned default value: {}'.format(self.data_file, default))
+            return default
+
+
+class GeneralSchema(Serialization, collections.MutableMapping):
+    def __init__(self, data_file, main_key, **kwargs):
+        """General Schema (like an abstract class)
+        Create something like this, e.g.:
+        {'video.mp4': {
+            'date': '01-01-2017',
+            'media': ['pic1.jpg', 'pic2.png', 'vid.mp4']
+            }
+        }
+        end e.g. Where 'video.mp4' is the main key and its value are kwargs
+        """
+        Serialization.__init__(self, data_file)
+        self.main_key = main_key
+        self.data[main_key] = {}
+
+        self.data[main_key].update(kwargs)
+
+    serialize = Serialization.serialize_as_json  # alias, new ref
+    deserialize = Serialization.deserialize_from_json  # alias, new ref
+
+    def __iter__(self):
+        """Convert this instance to a dictionary"""
+        for k, v in self.data.items():
+            yield k, v
+
+    def __getitem__(self, key):
+        """Square brackets operator overriding. e.g.: datum['main_key']"""
+        if key == 'main_key':
+            return self.main_key
+        return self.data[self.main_key][key]
+
+    def __setitem__(self, key, value):
+        if key == 'main_key':
+            self.main_key = value
+        self.data[self.main_key][key] = value
+
+    def __delitem__(self, key):
+        del self.data[key]
+
+    def __len__(self):
+        return len(self.data.keys())
+
+    def __eq__(self, other):
+        return self.data == other.data
+
+    def __hash__(self):
+        return hash(self.data)
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def __str__(self):
+        return pformat(dict(iter(self)))
+
+
+if __name__ == '__main__':
+    pass
